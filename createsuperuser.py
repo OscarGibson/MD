ef839f8b26c87d2d680e21a780a356b2a9ef03f8
Ostap Hyba <ostap.hyba@gmail.com>
2020-04-09 20:34:22 +0300

    debug

--- createsuperuser.py
+++ createsuperuser.py
@@ -11,5 +11,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 try:
     User.objects.create_superuser(os.environ('ADMIN_NAME'), os.environ('ADMIN_EMAIL'), os.environ('ADMIN_PASSWORD'))
-except Exception:
-    pass
+except Exception as exc:
+    print(exc)
+else:
+    print(os.environ('ADMIN_NAME'), os.environ('ADMIN_EMAIL'), os.environ('ADMIN_PASSWORD'))
