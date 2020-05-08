7e2bf3b858b8719e19961c746ec98f396bd3995d
Ostap Hyba <ostap.hyba@gmail.com>
2020-05-08 17:34:59 +0300

    changed photo field type

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,15 @@
+from cloudinary.models import CloudinaryField
 from django.conf import settings
 from django.db import models
 
 
 class Menu(models.Model):
     '''Menu model'''
-    photo = models.ImageField(f'{settings.UPLOADS_PATH}/menu')
+    photo = CloudinaryField(
+        f'{settings.UPLOADS_PATH}/menu',
+        blank=True,
+        null=True
+    )
     ordering = models.PositiveIntegerField(default=0, blank=False, null=False)
     name = models.CharField(max_length=32)
 
