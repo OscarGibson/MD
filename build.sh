e213d25c0171f3932db710d6f3b2ad5025cf7377
Ostap Hyba <ostap.hyba@gmail.com>
2020-05-13 15:27:02 +0300

    update static

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -f base/static/*
+rm -fr base/static/*
 rm -f base/templates/base/index.html
 cd ../frontend/maeasto-client && npm run build && \
 cp build/index.html ../../backend/base/templates/base/ && \
